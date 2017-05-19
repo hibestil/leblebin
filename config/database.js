11487d817afd10c7c8a7fd482e64ec6ce37cb157
Halil Ibrahim Bestil <halilibrahimbestil@hotmail.com>
2017-05-20 01:05:26 +0300

    Updated File Hierarchy

---
+++ config/database.js
@@ -0,0 +1,10 @@
+// config/database.js
+module.exports = {
+    'connection': {
+        'host': 'localhost',
+        'user': 'root',
+        'password': '123'
+    },
+	'database': 'my_schema',
+    'users_table': 'users'
+};
